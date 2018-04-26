a9a9f564f6eb855c8280f0c83668161b856335bd
shhsjcava <i@gmail.c>
2018-04-26 01:31:33 +0000

    step 6

--- flask-migrate.py
+++ flask-migrate.py
@@ -5,7 +5,6 @@ from flask_migrate import Migrate, MigrateCommand
 
 from app import app # we import the app object from the app module
 from app import db
-
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
